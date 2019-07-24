48aafd750cb6f856014ab9bc0d4198db364265c2
running1017 <R20979@kajima.co.jp>
2019-07-24 11:44:19 +0900

    log unicode

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ print("Hello Jenkins")
 
 t = datetime.datetime.now()
 
-log_text = "こんにちはJenkinsさん、今日はいい天気ですね。"
+log_text = u"こんにちはJenkinsさん、今日はいい天気ですね。"
 
 with open(t.strftime("%Y%m%d%H%M%S") + ".log", 'w') as f:
     f.write(log_text)
