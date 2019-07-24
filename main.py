cd33de7a313cb23fa7de3ceee1454be4a06313ef
running1017 <R20979@kajima.co.jp>
2019-07-24 11:42:41 +0900

    log message

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 # -*- coding: utf-8 -*-
 
 import __future__
+import datetime
 
 print("Hello Jenkins")
+
+t = datetime.datetime.now()
+
+log_text = "こんにちはJenkinsさん、今日はいい天気ですね。"
+
+with open(t.strftime("%Y%m%d%H%M%S") + ".log", 'w') as f:
+    f.write(log_text)
