b65cf861f7c47a385703d299c4432e8e98f7aa1f
running1017 <R20979@kajima.co.jp>
2019-07-24 13:17:44 +0900

    ksgi

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ print("Hello Jenkins")
 
 t = datetime.datetime.now()
 
-log_text = "Hi, Mr.Ogawa.\nIt is great cloudy day!"
+log_text = "Hi, Mr.Kasugai.\nIt is great cloudy day!"
 
 with open(t.strftime("%Y%m%d%H%M%S") + ".log", 'w') as f:
     f.write(log_text)
